54070c99b993b4d80fce316234a0316aef1a88c1
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-15 18:13:34 +0100

    GameService> playGame(), updatePlayerName()

--- src/main/java/cat/itacademy/s05/t01/n01/service/GameService.java
+++ src/main/java/cat/itacademy/s05/t01/n01/service/GameService.java
@@ -1,12 +1,15 @@
 package cat.itacademy.s05.t01.n01.service;
 
+import cat.itacademy.s05.t01.n01.model.Action;
 import cat.itacademy.s05.t01.n01.model.Game;
 import cat.itacademy.s05.t01.n01.model.GamePlayer;
+import cat.itacademy.s05.t01.n01.model.Player;
 import reactor.core.publisher.Mono;
 
 public interface GameService {
     Mono<Game> createGame(String name);
     Mono<Game> getGame(String id);
     Mono<Void> deleteGame(String gameId);
-    Mono<GamePlayer> updateName(String newName, GamePlayer gamePlayer);
+    Mono<Game> playGame(String gameId, Action action, int betAmount);
+    Mono<Game> updatePlayerName(String gameId, Player player);
 }
