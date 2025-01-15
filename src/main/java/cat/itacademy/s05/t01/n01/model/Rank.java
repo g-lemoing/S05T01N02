416c9fc192ac1788ba7a87850439d1aca23d136c
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-15 18:19:11 +0100

    Rank> modify ace value 1 --> 11

--- src/main/java/cat/itacademy/s05/t01/n01/model/Rank.java
+++ src/main/java/cat/itacademy/s05/t01/n01/model/Rank.java
@@ -1,6 +1,6 @@
 package cat.itacademy.s05.t01.n01.model;
 
-public enum Rank {_1("1", 1), _2("2", 2), _3("3", 3), _4("4", 4),
+public enum Rank {_1("1", 11), _2("2", 2), _3("3", 3), _4("4", 4),
                     _5("5", 5), _6("6", 6), _7("7", 7),
                     _8("8", 8), _9("9", 9), _10("10", 10),
                     _J("J", 10), _Q("Q", 10), _K("K", 10);
@@ -13,4 +13,11 @@ public enum Rank {_1("1", 1), _2("2", 2), _3("3", 3), _4("4", 4),
         this.value = value;
     }
 
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
