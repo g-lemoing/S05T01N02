d8a0fb72e3e92bed8b0604c2c1a858ca913682a5
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-15 18:21:18 +0100

    SwaggerConfig> replace Bean Docket api() with GroupedOpenApi

--- src/main/java/cat/itacademy/s05/t01/n01/swagger/SwaggerConfig.java
+++ src/main/java/cat/itacademy/s05/t01/n01/swagger/SwaggerConfig.java
@@ -1,25 +1,17 @@
 package cat.itacademy.s05.t01.n01.swagger;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.RequestHandler;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
-                .paths(PathSelectors.any())
+    GroupedOpenApi api(){
+        return GroupedOpenApi.builder()
+                .group("webflux-api")
+                .pathsToMatch("/game/**", "/player/**", "/ranking")
                 .build();
     }
 }
