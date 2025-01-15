0cc8aef0157a889e7e5bb9b9de63626c24cd85f7
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-15 18:14:34 +0100

    GameServiceImpl> playGame(), updatePlayerName(), playAction(), playBet(), PlayHit(), playStand()

--- src/main/java/cat/itacademy/s05/t01/n01/service/GameServiceImpl.java
+++ src/main/java/cat/itacademy/s05/t01/n01/service/GameServiceImpl.java
@@ -1,14 +1,12 @@
 package cat.itacademy.s05.t01.n01.service;
 
 import cat.itacademy.s05.t01.n01.exception.GameNotFoundException;
-import cat.itacademy.s05.t01.n01.model.Game;
-import cat.itacademy.s05.t01.n01.model.GamePlayer;
+import cat.itacademy.s05.t01.n01.model.*;
 import cat.itacademy.s05.t01.n01.repository.GameRepository;
 import cat.itacademy.s05.t01.n01.repository.PlayerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -19,7 +17,8 @@ public class GameServiceImpl implements GameService{
     private PlayerRepository playerRepository;
     @Autowired
     private GameRepository gameRepository;
-    @Autowired PlayerServiceImpl playerService;
+    @Autowired
+    PlayerServiceImpl playerService;
 
     private static Logger log = LoggerFactory.getLogger(PlayerServiceImpl.class);
 
@@ -30,7 +29,6 @@ public class GameServiceImpl implements GameService{
         return this.playerService.createNewPlayer(name).flatMap(
                 player -> {
                     GamePlayer gamePlayer = new GamePlayer(player);
-                    log.info(String.valueOf(player.getName()));
                     Game game = new Game(gamePlayer);
                     return gameRepository.save(game).doOnNext(
                             createdGame -> log.info("Game successfully initialized with id: "
@@ -54,7 +52,86 @@ public class GameServiceImpl implements GameService{
     }
 
     @Override
-    public Mono<GamePlayer> updateName(String newName, GamePlayer gamePlayer) {
+    public Mono<Game> playGame(String gameId, Action action, int betAmount) {
+        return getGame(gameId).flatMap(game -> {
+            if(game.isGameOver()){
+                return Mono.error(new IllegalStateException("This game is over."));
+            }
+            return playAction(game, action, betAmount);
+        });
+    }
+
+    @Override
+    public Mono<Game> updatePlayerName(String gameId, Player player) {
         return null;
+    }
+
+    public Mono<Game> playAction (Game game, Action action, int betAmount){
+        switch (action){
+            case BET -> {
+                return playBet(game, betAmount);
+            }
+            case HIT -> {
+                return playHit(game);
+            }
+            case DOUBLE -> {
+
+            }
+            case SECURE -> {
+
+            }
+            case STAND -> {
+                return playStand(game);
+            }
+            case null, default -> {
+                return Mono.error(new IllegalArgumentException("Invalid move"));
+            }
+        }
+        return null;
+    }
+
+    Mono<Game> playBet(Game game, int betAmount){
+        if(game.getGameStatus() != GameStatus.INIT) throw new IllegalArgumentException("You cannot bet at this moment.");
+        if(betAmount == 0) throw new IllegalArgumentException("Bet amount must be greater than 0.");
+
+        game.setBetAmount(betAmount);
+        game.initialCardDeal();
+        if(Game.getHandValue(game.getGamePlayer().getPlayerHand()) > 21){
+            game.setGameStatus(GameStatus.BANK_WINS);
+        } else {
+            game.setGameStatus(GameStatus.SECOND_ROUND);
+        }
+        return gameRepository.save(game);
+    }
+
+    Mono<Game> playHit(Game game){
+        if(game.getGameStatus() != GameStatus.SECOND_ROUND) throw new IllegalArgumentException("You cannot hit for another card at this moment.");
+
+        game.givePlayerHand(1);
+        if(Game.getHandValue(game.getGamePlayer().getPlayerHand()) > 21){
+            game.setGameStatus(GameStatus.BANK_WINS);
+        }
+        return gameRepository.save(game);
+    }
+
+    Mono<Game> playStand(Game game) {
+        if (game.getGameStatus() != GameStatus.SECOND_ROUND)
+            throw new IllegalArgumentException("You cannot stand at this moment.");
+
+        while (Game.getHandValue(game.getBankHand()) < 17) {
+            game.giveBankHand(1);
+        }
+        GameStatus gameStatus = game.ckeckWinner();
+        game.setGameStatus(gameStatus);
+        if (gameStatus == GameStatus.PLAYER_WINS || gameStatus == GameStatus.DRAW) {
+            double prizeAmount = game.getplayerPrize();
+            return this.playerService.updatePlayerScore(game.getGamePlayer().getPlayer(), prizeAmount)
+                    .flatMap(player -> {
+                        log.info("Player " + player.getName() + " wins the game! Prize amount: "
+                                + prizeAmount + ", new score: " + player.getScore());
+                        return gameRepository.save(game);
+                    });
+        }
+        return gameRepository.save(game);
     }
 }
