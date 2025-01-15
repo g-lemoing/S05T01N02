8f98d76402df929495693961c4725dcb40b9ffe4
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-15 18:22:00 +0100

    S05T01N01Application> add OpenAPIDefinition annotation

--- src/main/java/cat/itacademy/s05/t01/n01/S05T01N01Application.java
+++ src/main/java/cat/itacademy/s05/t01/n01/S05T01N01Application.java
@@ -1,9 +1,11 @@
 package cat.itacademy.s05.t01.n01;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class S05T01N01Application {
 
 	public static void main(String[] args) {
